ad20371173c745673ab9d5117c46e841f81132d1
Chioma <chiomajoy076@gmail.com>
2025-08-30 16:39:25 +0100

    this is my first commit message

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace JOYMVC2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
